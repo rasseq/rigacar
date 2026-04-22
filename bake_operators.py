8d6cb62d6df9a67bc10856c63e18dbd3b981caa9
rasseq <149048878+rasseq@users.noreply.github.com>
2026-04-23 02:18:36 +0300

    Handle slotted actions when reading/writing F-curves

--- bake_operators.py
+++ bake_operators.py
@@ -25,6 +25,39 @@ import itertools
 import re
 
 
+def get_action_fcurves(action, animated_object, ensure=False):
+    """
+    Return an F-Curve collection for both legacy and slotted actions.
+    """
+    if action is None:
+        return None
+
+    if hasattr(action, 'fcurves'):
+        return action.fcurves
+
+    try:
+        from bpy_extras import anim_utils
+    except Exception:
+        return None
+
+    animation_data = animated_object.animation_data if animated_object else None
+    action_slot = getattr(animation_data, 'action_slot', None) if animation_data else None
+    if action_slot is None and hasattr(action, 'slots') and action.slots:
+        action_slot = action.slots.active or action.slots[0]
+    if action_slot is None:
+        return None
+
+    channelbag = None
+    if ensure and hasattr(anim_utils, 'action_ensure_channelbag_for_slot'):
+        channelbag = anim_utils.action_ensure_channelbag_for_slot(action, action_slot)
+    elif hasattr(anim_utils, 'action_get_channelbag_for_slot'):
+        channelbag = anim_utils.action_get_channelbag_for_slot(action, action_slot)
+    elif hasattr(action, 'fcurve_ensure_for_datablock'):
+        channelbag = action.fcurve_ensure_for_datablock(animated_object)
+
+    return channelbag.fcurves if channelbag is not None else None
+
+
 def cursor(cursor_mode):
     def cursor_decorator(func):
         def wrapper(self, context, *args, **kwargs):
@@ -77,16 +110,21 @@ def clear_property_animation(context, property_name, remove_keyframes=True):
     if remove_keyframes and context.object.animation_data and context.object.animation_data.action:
         fcurve_datapath = '["%s"]' % property_name
         action = context.object.animation_data.action
-        fcurve = action.fcurves.find(fcurve_datapath)
+        fcurves = get_action_fcurves(action, context.object)
+        if fcurves is None:
+            context.object[property_name] = .0
+            return
+        fcurve = fcurves.find(fcurve_datapath)
         if fcurve is not None:
-            action.fcurves.remove(fcurve)
+            fcurves.remove(fcurve)
     context.object[property_name] = .0
 
 
 def create_property_animation(context, property_name):
     action = context.object.animation_data.action
     fcurve_datapath = '["%s"]' % property_name
-    return action.fcurves.new(fcurve_datapath, index=0, action_group='Wheels rotation')
+    fcurves = get_action_fcurves(action, context.object, ensure=True)
+    return fcurves.new(fcurve_datapath, index=0, action_group='Wheels rotation') if fcurves is not None else None
 
 
 class FCurvesEvaluator(object):
@@ -174,25 +212,30 @@ class BakingOperator(object):
 
     def _create_euler_evaluator(self, action, source_bone):
         fcurve_name = 'pose.bones["%s"].rotation_euler' % source_bone.name
-        fc_root_rot = [action.fcurves.find(fcurve_name, index=i) for i in range(3)]
+        fcurves = get_action_fcurves(action, self._animated_object)
+        fc_root_rot = [fcurves.find(fcurve_name, index=i) if fcurves is not None else None for i in range(3)]
         return EulerToQuaternionFCurvesEvaluator(FCurvesEvaluator(fc_root_rot, default_value=(.0, .0, .0)))
 
     def _create_quaternion_evaluator(self, action, source_bone):
         fcurve_name = 'pose.bones["%s"].rotation_quaternion' % source_bone.name
-        fc_root_rot = [action.fcurves.find(fcurve_name, index=i) for i in range(4)]
+        fcurves = get_action_fcurves(action, self._animated_object)
+        fc_root_rot = [fcurves.find(fcurve_name, index=i) if fcurves is not None else None for i in range(4)]
         return QuaternionFCurvesEvaluator(FCurvesEvaluator(fc_root_rot, default_value=(1.0, .0, .0, .0)))
 
     def _create_location_evaluator(self, action, source_bone):
         fcurve_name = 'pose.bones["%s"].location' % source_bone.name
-        fc_root_loc = [action.fcurves.find(fcurve_name, index=i) for i in range(3)]
+        fcurves = get_action_fcurves(action, self._animated_object)
+        fc_root_loc = [fcurves.find(fcurve_name, index=i) if fcurves is not None else None for i in range(3)]
         return VectorFCurvesEvaluator(FCurvesEvaluator(fc_root_loc, default_value=(.0, .0, .0)))
 
     def _create_scale_evaluator(self, action, source_bone):
         fcurve_name = 'pose.bones["%s"].scale' % source_bone.name
-        fc_root_loc = [action.fcurves.find(fcurve_name, index=i) for i in range(3)]
+        fcurves = get_action_fcurves(action, self._animated_object)
+        fc_root_loc = [fcurves.find(fcurve_name, index=i) if fcurves is not None else None for i in range(3)]
         return VectorFCurvesEvaluator(FCurvesEvaluator(fc_root_loc, default_value=(1.0, 1.0, 1.0)))
 
     def _bake_action(self, context, *source_bones):
+        self._animated_object = context.object
         action = context.object.animation_data.action
         nla_tweak_mode = context.object.animation_data.use_tweak_mode if hasattr(context.object.animation_data, 'use_tweak_mode') else False
 
@@ -301,6 +344,8 @@ class ANIM_OT_carWheelsRotationBake(bpy.types.Operator, BakingOperator):
 
     def _bake_wheel_rotation(self, context, baked_action, bone, brake_bone):
         fc_rot = create_property_animation(context, bone.name.replace('MCH-', ''))
+        if fc_rot is None:
+            return
 
         for f, distance in self._evaluate_distance_per_frame(baked_action, bone, brake_bone):
             kf = fc_rot.keyframe_points.insert(f, distance)
@@ -377,6 +422,9 @@ class ANIM_OT_carSteeringBake(bpy.types.Operator, BakingOperator):
         clear_property_animation(context, 'Steering.rotation')
         fix_old_steering_rotation(context.object)
         fc_rot = create_property_animation(context, 'Steering.rotation')
+        if fc_rot is None:
+            self.report({'ERROR'}, 'Cannot create steering animation channels for this action type')
+            return
         action = self._bake_action(context, bone)
 
         try:
